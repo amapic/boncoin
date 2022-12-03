2075118c1d7248456168d051b342efc9c90d0e5c
amapic <amaury.pichat@gmail.com>
2022-12-03 23:24:50 +0000

    Update 5 files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,29 @@
 // import App from 'next/app'
-import '../index.scss';
-import '../App.scss';
-import '../style/planet.scss';
+import "../index.scss";
+// import '../App.scss';
+import "../style/planet.scss";
 // import '../style/navbar.scss';
-import '../style/navbar2.scss';
-import '../style/bon_coin.scss';
-import '../style/radio.scss';
-import '../style/custom.scss';
-import '../style/switch.scss';
-import 'bootstrap/dist/css/bootstrap.css'
+import "../style/navbar2.scss";
+import "../style/bon_coin.scss";
+import "../style/radio.scss";
+// import "../style/custom.scss";
+import "../style/switch.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
-import { motion } from 'framer-motion';
-import shortid from 'shortid';
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
+import { motion } from "framer-motion";
+import shortid from "shortid";
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
 // import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -31,22 +31,30 @@ export default function MyApp(props) {
 
   return (
     // <React.Fragment>
-    <motion.div key={shortid.generate()} initial="pageInitial" animate="pageAnimate" variants={{
-      pageInitial: {
-        opacity: 0
-      },
-      pageAnimate: {
-        opacity: 1
-      },
-    }}>
+    <motion.div
+      key={shortid.generate()}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
       <Head>
         <title>Dashboard vaccination</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       {/* <ThemeProvider theme={theme}> */}
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+      {typeof window === "undefined" ? null : <Component {...pageProps} />}
       {/* </ThemeProvider> */}
     </motion.div>
     // </React.Fragment>
